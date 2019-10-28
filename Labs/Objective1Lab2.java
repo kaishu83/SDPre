14ac85ddc45987943e55f66f45a50f2529195a17
kaishu83 <kaishu83@gmail.com>
2019-10-28 10:48:59 -0600

    Finished Lab1

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,5 @@
+public class Objective1Lab2{
+  public static void main(String[] args) {
+    System.out.println("Kai Shu, 36");
+  }
+}
---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3{
+  public static void main(String[]args){
+    System.out.println("Name : Kai Shu");
+    System.out.println("Age :36");
+    System.out.println("State : N/A(China)");
+  }
+}
